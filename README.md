5f5d016a56dbf3d886a8395339d358bcf3dcea65
Ian Mooney <iancmooney@gmail.com>
2013-10-06 17:47:58 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+### 1. Readable
+  
+  ./run.rb readable
+
+### 2. Fast
+
+  ./run.rb fast
+  
+### 3. Extenible
+
+  ./run.rb fast [word length] [text file]
+  
+  e.g.
+  
+  ./run.rb 5 lib/WordList-sample.txt
+  
+  
--- run.rb
+++ run.rb
@@ -2,6 +2,10 @@
 
 require './lib/boot.rb'
 
-# FastFinder.concatenated_words
-# Readable::Word.print_words
-Extendible::Word.print_words(:length => ARGV[0], :file_name => ARGV[1])
+if ARGV[0] == 'readable'
+  Readable::Word.print_words
+elsif ARGV[0] == 'fast'
+  FastFinder.concatenated_words
+else
+  Extendible::Word.print_words(:length => ARGV[0], :file_name => ARGV[1])
+end
